8ac062b7c46e27c56910b98c705e844223c31d80
k9ne257 <victorien.ngassam@yahoo.com>
2024-03-05 18:59:30 +0100

    first commit

---
+++ src/main/java/com/webfrey/gastroSmart/GastroSmartApplication.java
@@ -0,0 +1,13 @@
+package com.webfrey.gastroSmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastroSmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastroSmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webfrey/gastroSmart/GastroSmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webfrey.gastroSmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastroSmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
